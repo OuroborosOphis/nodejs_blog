ede827af2a8cd286e748ae4fd3868e581071e2db
OuroborosOphis <trunghieu3132001@gmail.com>
2024-08-30 11:54:15 +0700

    test auto format

--- start/blog/src/index.js
+++ start/blog/src/index.js
@@ -1,45 +1,43 @@
 const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
-const app = express();
-const port = 3000;
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars')
+const app = express()
+const port = 3000
 
-const route = require('./routes'); // index.js trong routes se tu dong duoc require
+const route = require('./routes')  // index.js trong routes se tu dong duoc require 
 
 // /img/logo.png -> se tim trong public/img/logo.png va hien thi ra
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Middleware
 // dung cho gui form
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-);
-// dung cho gui json tu client len server (axios, fetch)
+app.use(express.urlencoded({
+  extended: true
+}));
+// dung cho gui json tu client len server (axios, fetch) 
 app.use(express.json());
 
 // Http logger
-app.use(morgan('combined'));
+app.use(morgan('combined'))
 
 // Template engine
-app.engine(
-    'hbs',
-    handlebars.engine({
-        extname: '.hbs',
-    }),
-);
-app.set('view engine', 'hbs');
-        app.set('views', path.join(__dirname, 'resources/views'));
+      app.engine(
+  'hbs',  handlebars.engine({
+  extname: 
+    '.hbs'
+}));
+app.set('view engine', 
+  'hbs');
+  app.set('views', path.join(__dirname, 'resources/views'));
 
 // console.log('PATH: ', path.join(__dirname, 'resources/views'));
 
 // Routes init
-route(    app); // dung route trong index.js
+route(  app);  // dung route trong index.js
 
 // 127.0.0.1 is the local IP address (localhost)
 
-        app.listen(port, () => {
-            console.log(`Example app listening at http://localhost:${port}`);
-        });
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
