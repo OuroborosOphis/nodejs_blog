0f17b63ed1c054b9b699132c4d2ad3b564e1ccbe
OuroborosOphis <trunghieu3132001@gmail.com>
2024-08-30 19:12:57 +0700

    change router.use -> get

--- start/blog/src/routes/news.js
+++ start/blog/src/routes/news.js
@@ -4,10 +4,10 @@ const router = express.Router()
 const newsController = require('../app/controllers/NewsController')
 
 // khi nguoi dung truy cap vao /:slug thi se chay vao function handler show trong newsController 
-router.use('/:slug', newsController.show)
+router.get('/:slug', newsController.show)  // doi tu use thanh get 
 
 // tuyen goc la /news can dua xuong cuoi cung de tranh truong hop /news/:slug bi hien thi truoc
-router.use('/', newsController.index)
+router.get('/', newsController.index)
 
 // khi co trang lien quan den news thi se dung router nay de xu ly 
 // function handler thi viet trong newsController
