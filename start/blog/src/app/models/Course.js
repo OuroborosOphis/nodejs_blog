a5a332a75c9cf49ff4f63577e038fc0941b05896
OuroborosOphis <trunghieu3132001@gmail.com>
2024-08-31 20:37:25 +0700

    create course

--- start/blog/src/app/models/Course.js
+++ start/blog/src/app/models/Course.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
-    name: {type: String, maxLength: 255},
-    description: {type: String, maxLength: 600},
-    image: {type: String, maxLength: 255},
-    createAt: {type: Date, default: Date.now},
-    updateAt: {type: Date, default: Date.now},
+    name: {type: String, required: true},
+    description: {type: String},
+    image: {type: String},
+    level: {type: String},
+    slug: {type: String, slug: "name", unique: true},
+    videoId: {type: String, required: true},
+  }, {
+    timestamps: true,  // tu dong tao ra 2 field la createdAt va updatedAt
   });
 
 module.exports = mongoose.model('Course', Course); // Course la ten cua model, Course la schema
